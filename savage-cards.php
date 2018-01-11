f40de95d644898806e26e63229e32a0f6f15196a
Trine <trine@dekode.no>
2018-01-11 14:30:58 +0100

    Remove dev from version

--- savage-cards.php
+++ savage-cards.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/dekodeinteraktiv/savage-cards
  * GitHub Plugin URI: https://github.com/dekodeinteraktiv/savage-cards
  * Description: Card setup plugin
- * Version: 1.0.0-dev
+ * Version: 1.0.0
  * Author: Dekode
  * Author URI: https://dekode.no
  * License: GPL-3.0
