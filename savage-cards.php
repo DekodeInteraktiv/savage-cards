28b18e21931d9657aead58d1d7727c7942cad921
Trine <trine@dekode.no>
2018-02-06 12:52:16 +0100

    Bump version

--- savage-cards.php
+++ savage-cards.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/dekodeinteraktiv/savage-cards
  * GitHub Plugin URI: https://github.com/dekodeinteraktiv/savage-cards
  * Description: Card setup plugin
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Dekode
  * Author URI: https://dekode.no
  * License: GPL-3.0-or-later
