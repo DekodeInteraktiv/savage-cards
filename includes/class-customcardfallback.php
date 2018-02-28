39a5a1ca73ef02d4e852177afd3e816580757678
Petter Walbø Johnsgård <petter@dekode.no>
2018-02-28 13:46:11 +0100

    Add missing type declaration

--- includes/class-customcardfallback.php
+++ includes/class-customcardfallback.php
@@ -43,7 +43,7 @@ if ( ! class_exists( '\\Dekode\\Savage\\CustomCardFallback' ) && class_exists( '
 		 *
 		 * @param array $args Component args.
 		 */
-		public function link( $args ) {
+		public function link( array $args ) {
 			$link = get_post_meta( $args['id'], 'card_link', true );
 			savage_card_component( 'link', $link );
 		}
