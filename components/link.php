08e9bd46e7351e7a24ff84e478c6ab2d353d0d23
Stian Øveråsen <stian@dekode.no>
2018-04-17 15:54:30 +0200

    Add aria-label

--- components/link.php
+++ components/link.php
@@ -22,8 +22,9 @@ if ( empty( $args['url'] ) ) {
 	return;
 }
 
-printf( '<a href="%s" class="savage-card-link"%s><span class="screen-reader-text">%s</span></a>',
+printf( '<a href="%s" class="savage-card-link"%s" aria-label="%s"><span class="screen-reader-text">%s</span></a>',
 	esc_url( $args['url'] ),
 	! empty( $args['target'] ) ? sprintf( ' target="%s"', esc_attr( $args['target'] ) ) : '',
+	esc_attr( $args['title'] ),
 	esc_html( $args['title'] )
 );
